957e5634b8001875ffc1393e7297e2be928f2200
DZaCKi <bougherrazaki@gmail.com>
2022-12-27 22:18:57 +0100

    code

---
+++ src/test/java/com/example/gitcicdaction/GitCicdActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcicdaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCicdActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
